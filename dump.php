260bd4101387ad8fc641270770c3ae53136dcb21
Andriyzubets <zubets.andriy@gmail.com>
2022-11-29 16:08:09 +0200

    third-1

--- dump.php
+++ dump.php
@@ -14,9 +14,10 @@ $dir = dirname(__FILE__) . '/db/dump.sql';
 
 echo "<h3>Backing up database to `<code>{$dir}</code>`</h3>";
 if(isset($_GET['return']) and $_GET['return'] == 1) {
-	exec("mysql --user={$user} --password={$pass} --host={$host} {$database} < {$dir}", $output);
+	exec("mysql -u {$user} --p{$pass} {$database} < {$dir}", $output);
 } else {
-	exec("mysqldump --user={$user} --password={$pass} --host={$host} {$database} --result-file={$dir} 2>&1", $output);
+	$output = 'sec';
+	#exec("mysqldump --user={$user} --password={$pass} --host={$host} {$database} --result-file={$dir} 2>&1", $output);
 }
 
 var_dump($output);
