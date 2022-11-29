c9a6a80b806a3ed5db3c653ddbd7891769902210
Andriyzubets <zubets.andriy@gmail.com>
2022-11-29 16:36:16 +0200

    test merging

--- dump.php
+++ dump.php
@@ -14,7 +14,28 @@ $dir = dirname(__FILE__) . '/db/dump.sql';
 
 echo "<h3>Backing up database to `<code>{$dir}</code>`</h3>";
 if(isset($_GET['return']) and $_GET['return'] == 1) {
-	exec("mysql -u {$user} --p{$pass} {$database} < {$dir}", $output);
+	$output = 'fir';
+
+	$conn =new mysqli($host,$user,$pass,$database);
+
+	$query = '';
+	$sqlScript = file($dir);
+	foreach ($sqlScript as $line)	{
+
+		$startWith = substr(trim($line), 0 ,2);
+		$endWith = substr(trim($line), -1 ,1);
+
+		if (empty($line) || $startWith == '--' || $startWith == '/*' || $startWith == '//') {
+			continue;
+		}
+		
+		$query = $query . $line;
+		if ($endWith == ';') {
+			mysqli_query($conn,$query) or die('<div class="error-response sql-import-response">Problem in executing the SQL query <b>' . $query. '</b></div>');
+			$query= '';
+		}
+	}
+	echo '<div class="success-response sql-import-response">SQL file imported successfully</div>';
 } else {
 	$output = 'sec';
 	exec("mysqldump --user={$user} --password={$pass} --host={$host} {$database} --result-file={$dir} 2>&1", $output);
