25770473c75477478af2a453a1b77f4b67e7199e
Andriyzubets <zubets.andriy@gmail.com>
2022-11-29 16:01:29 +0200

    ÃaddScript

--- dump.php
+++ dump.php
@@ -13,7 +13,10 @@ $host = DB_HOST;
 $dir = dirname(__FILE__) . '/db/dump.sql';
 
 echo "<h3>Backing up database to `<code>{$dir}</code>`</h3>";
-
-exec("mysqldump --user={$user} --password={$pass} --host={$host} {$database} --result-file={$dir} 2>&1", $output);
+if(isset($_GET['return']) and $_GET['return'] == 1) {
+	exec("mysql --user={$user} --password={$pass} --host={$host} {$database} < {$dir}", $output);
+} else {
+	exec("mysqldump --user={$user} --password={$pass} --host={$host} {$database} --result-file={$dir} 2>&1", $output);
+}
 
 var_dump($output);
